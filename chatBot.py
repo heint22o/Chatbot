0dd6186295208cedf9649d0118167fdb83140db4
heint22o <heint22o@mtholyoke.edu>
2020-04-16 18:17:48 -0400

    Added some more neuro tags

--- chatBot.py
+++ chatBot.py
@@ -77,7 +77,7 @@ net = tflearn.regression(net)
 model = tflearn.DNN(net)
 
 
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.fit(training, output, n_epoch=1500, batch_size=8, show_metric=True)
 model.save("model.tflearn")
 
 
